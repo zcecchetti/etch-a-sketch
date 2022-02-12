946f27798cc4b2ce7640846e34685a7e9ca7452c
Zachary Cecchetti <zcecchetti@gmail.com>
2022-02-11 16:29:22 -0800

    Added resetToZero function to reset elements to 0 when creating new display dimensions

--- javascript.js
+++ javascript.js
@@ -1,14 +1,20 @@
 
-// Get user input height and width values from text box
+// Get user input height and width values from text box and create
 
-const displayHeight = document.getElementById("height");
-const displayWidth = document.getElementById("width");
-const divContainer = document.getElementById("divContainer");
-const row = document.querySelector(".row");
+const generate = document.querySelector("#generate");
+generate.addEventListener("click", () => {
+    const displayHeight = document.getElementById("height").value;
+    const displayWidth = document.getElementById("width").value;  
 
-
+    createDisplay(displayWidth, displayHeight);
+});
 
 function createDisplay(displayWidth, displayHeight) {
+
+    resetToZero();
+    const divContainer = document.getElementById("divContainer");
+    const row = document.createElement("div");
+    row.classList.add("row");
 
     for (let width = 0; width < displayWidth; width++) {
         const div = document.createElement('div');
@@ -16,8 +22,18 @@ function createDisplay(displayWidth, displayHeight) {
         row.appendChild(div);
     };
 
-    for (let height = 1; height < displayHeight; height++) {
+    for (let height = 0; height < displayHeight; height++) {
         const rowClone = row.cloneNode(true);
         divContainer.appendChild(rowClone);
     }
+};
+
+// Reset number of elements in container to 0
+
+function resetToZero() {
+    const divContainer = document.getElementById("divContainer");
+
+    while (divContainer.firstChild) {
+        divContainer.removeChild(divContainer.firstChild);
+    };
 };
